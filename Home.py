a62d6acb5ce1e2b78da3aba5304368ff46596281
Gabriel018 <duke.comandos@gmail.com>
2022-04-26 21:13:31 -0300

    requirements

---
+++ Home.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+from flask_sqlalchemy import  SQLAlchemy
+
+app = Flask(__name__)
+
+db = SQLAlchemy(app)
+
+
+@app.route("/")
+
+def home():
+
+    return render_template('home.html')
+
+
+@app.route("/prod")
+
+def prod():
+
+    return "Pagina de produtos"
+
+app.run(debug=True)
